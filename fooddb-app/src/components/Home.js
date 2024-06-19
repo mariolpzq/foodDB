d82dff447cb06e60b87cd828ef43ae2d8331b5ea
Mario <lopezquesadamario@gmail.com>
2024-06-20 01:17:17 +0200

    redireccion pagina principal

--- fooddb-app/src/components/Home.js
+++ fooddb-app/src/components/Home.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import { useContext } from 'react';
+import AuthContext from '../Auth';
+
+
 
 function Home() {
+  const { isAuthenticated } = useContext(AuthContext);
+  let redireccion;
+
+  if (!isAuthenticated) {
+    redireccion = '/login';
+  } else {
+    redireccion = '/recetas';
+  }
+
   return (
     <div className='home'>
       <h1>¡Bienvenido a FoodDB!</h1>
-      <button className='button-style' onClick={() => window.location.href = '/recetas'}>Descubre nuestras recetas</button>
+      <button className='button-style' onClick={() => window.location.href = redireccion}>Descubre nuestras recetas</button>
     </div>
   );
 }
